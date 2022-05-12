b1ba8efe916d95be3ebdb2e58f5596951b71df6c
isseyboulet-tongier <issey.boulet-tongier@efrei.net>
2022-05-12 18:12:49 +0200

    Initial commit

---
+++ MVS_FizzBuzz.h
@@ -0,0 +1,11 @@
+//
+// Created by Issey Boulet-Tongier on 12/05/2022.
+//
+
+#ifndef TP_3_MVS_FIZZBUZZ_H
+#define TP_3_MVS_FIZZBUZZ_H
+
+#define FIZZ "Fizz"
+#define BUZZ "Buzz"
+
+#endif //TP_3_MVS_FIZZBUZZ_H
